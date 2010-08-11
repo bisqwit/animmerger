60a693ae5c2455a90483946bf622a515de909bc3
Joel Yliluoma <joel.yliluoma@iki.fi>
2010-08-11 16:07:14 +0300

    Improve TinyAverage accuracy without increasing its size

--- pixels/tinyaveragepixel.hh
+++ pixels/tinyaveragepixel.hh
@@ -3,30 +3,39 @@ class TinyAveragePixel: public Base
 {
     unsigned result, n;
 public:
-    TinyAveragePixel() : result(DefaultPixel), n(0)
+    TinyAveragePixel() : result(0), n(0)
     {
+        // "result" stores the average as such:
+        //    R in 10 bits
+        //    G in 12 bits
+        //    B in 10 bits
     }
     void set(uint32 p, unsigned timer=0) FasterPixelMethod
     {
         Base::set(p, timer);
-        set_rgb((p>>16)&0xFF, (p>>8)&0xFF, p&0xFF);
+        //     10987654321098765432109876543210
+        // in: 00000000RRRRRRRRGGGGGGGGBBBBBBBB
+        // out:                        BBBBBBBBbb
+        //                     GGGGGGGGgggg
+        //             RRRRRRRRrr
+        set_n_widergb( (p&0xFF0000u) >> 14,
+                       (p&0xFF00u) >> 4,
+                       (p&0xFFu)<<2, 1);
     }
     void set_n(uint32 p, unsigned count) FasterPixelMethod
     {
-        set_n_rgb((p>>16)&0xFF, (p>>8)&0xFF, p&0xFF, count);
+        set_n_widergb( (p&0xFF0000u) >> 14,
+                       (p&0xFF00u) >> 4,
+                       (p&0xFFu)<<2, count);
     }
-    void set_rgb(unsigned R,unsigned G,unsigned B, unsigned=0) FastPixelMethod
+    void set_n_widergb(unsigned R,unsigned G,unsigned B, unsigned count) FastPixelMethod
     {
-        set_n_rgb(R,G,B, 1);
-    }
-    void set_n_rgb(unsigned R,unsigned G,unsigned B, unsigned count) FastPixelMethod
-    {
-        unsigned r = (result>>16)&0xFF, g = (result>>8)&0xFF, b = (result&0xFF);
-        r = (r*n+R*count) / (n+count);
-        g = (g*n+G*count) / (n+count);
-        b = (b*n+B*count) / (n+count);
+        unsigned r = (result>>22)&0x3FF, g = (result>>10)&0xFFF, b = (result&0x3FF);
+        r = (r*n + R*count) / (n+count);
+        g = (g*n + G*count) / (n+count);
+        b = (b*n + B*count) / (n+count);
         n += count;
-        result = (r<<16) | (g<<8) | (b<<0);
+        result = (r<<22) | (g<<10) | (b<<0);
     }
 
     inline uint32 get(unsigned=0) const FasterPixelMethod
@@ -36,7 +45,17 @@ public:
 
     inline uint32 GetTinyAverage(unsigned=0) const FasterPixelMethod
     {
-        return result;
+        if(!n) return DefaultPixel;
+        //     10987654321098765432109876543210
+        // in: RRRRRRRRrrGGGGGGGGggggBBBBBBBBbb
+        //out: 00000000RRRRRRRRGGGGGGGGBBBBBBBB
+        //                           ^^ = B rshift amount (2)
+        //               ^^^^^^ = G rshift amount (6)
+        //     ^^^^^^^^ = R rshift amount (8)
+        return ((result>>2) & 0x0000FFu) // b: drop 2 bits
+             | ((result>>6) & 0x00FF00u) // g: drop 4 bits (2+4=6)
+             | ((result>>8) & 0xFFFF00u) // r: drop 2 bits (2+4+2=8)
+        ;
     }
 /////////
     static const unsigned long Traits =
